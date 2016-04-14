bf706a950bf60370bb3d268f88bcf056d34e049c
Kris Nuttycombe <kris.nuttycombe@pellucid.com>
2016-04-14 16:26:43 -0600

    Improve extensions layout

--- src/thx/schema/SchemaExtensions.hx
+++ src/thx/schema/SchemaExtensions.hx
@@ -26,25 +26,27 @@ class ObjectSchemaExtensions {
   public static function contramap<N, O, A>(o: ObjectBuilder<O, A>, f: N -> O): ObjectBuilder<N, A> {
     return switch o {
       case Pure(a): Pure(a);
-      case Ap(s, k): Ap(contramapPS(s, f), contramap(k, f));
+      case Ap(s, k): Ap(PropSchemaExtensions.contramap(s, f), contramap(k, f));
     }
   }
 
-  public static function map<O, A, B>(s: ObjectBuilder<O, A>, f: A -> B): ObjectBuilder<O, B> {
-    return switch s {
+  public static function map<O, A, B>(o: ObjectBuilder<O, A>, f: A -> B): ObjectBuilder<O, B> {
+    return switch o {
       case Pure(a): Pure(f(a));
       case Ap(s, k): Ap(s, map(k, f.compose));
     };
   }
 
-  public static function ap<O, A, B>(s: ObjectBuilder<O, A>, f: ObjectBuilder<O, A -> B>): ObjectBuilder<O, B> {
+  public static function ap<O, A, B>(o: ObjectBuilder<O, A>, f: ObjectBuilder<O, A -> B>): ObjectBuilder<O, B> {
     return switch f {
-      case Pure(g): map(s, g);
-      case Ap(fs, fk): Ap(fs, ap(s, map(fk, flip)));
+      case Pure(g): map(o, g);
+      case Ap(s, k): Ap(s, ap(o, map(k, flip)));
     };
   }
+}
 
-  public static function contramapPS<N, O, A>(s: PropSchema<O, A>, f: N -> O): PropSchema<N, A>
+class PropSchemaExtensions {
+  public static function contramap<N, O, A>(s: PropSchema<O, A>, f: N -> O): PropSchema<N, A>
     return switch s {
       case Required(n, s, a): Required(n, s, a.compose(f));
       case Optional(n, s, a): Optional(n, s, a.compose(f));
